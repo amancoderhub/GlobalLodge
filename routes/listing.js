d09d3563ef25f9ff38af506e9532f09386947496
Aman Shrivastav <saurbhsrivastav6@gmail.com>
2025-07-23 17:39:50 +0530

    change

--- routes/listing.js
+++ routes/listing.js
@@ -5,6 +5,9 @@ const { listingSchema } = require("../schema.js");
 const ExpressError = require("../utils/ExpressError.js");
 const Listing = require("../models/listing.js");
 const flash = require("connect-flash");
+const session = require("express-session");
+const passport = require("passport");
+const {isLoggedIn} = require("../middleware.js");
 
 const validateListing = (req, res, next) => {
     let {error} = listingSchema.validate(req.body);
@@ -23,7 +26,7 @@ router.get("/", wrapAsync(async (req, res) => {
 }));
 
 //new Route
-router.get("/new", (req, res) => {
+router.get("/new", isLoggedIn, (req, res) => {
     res.render("listings/new.ejs");
 });
 
@@ -40,6 +43,7 @@ router.get("/:id", wrapAsync(async (req, res) => {
 
 //create route
 router.post("/",
+    isLoggedIn,
     validateListing,
     wrapAsync(async (req, res, next) => {
     const listingData = req.body.listing;
@@ -58,7 +62,7 @@ router.post("/",
 
 
 //Edit Route
-router.get("/:id/edit", wrapAsync(async (req, res) => {
+router.get("/:id/edit", isLoggedIn, wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
     if(!listing){
@@ -70,6 +74,7 @@ router.get("/:id/edit", wrapAsync(async (req, res) => {
 
 //update route 
 router.put("/:id",
+    isLoggedIn,
     validateListing,
     wrapAsync(async (req, res) => {
     let { id } = req.params;
@@ -88,7 +93,7 @@ router.put("/:id",
 
 
 //delete route
-router.delete("/:id", wrapAsync(async (req, res) => {
+router.delete("/:id", isLoggedIn, wrapAsync(async (req, res) => {
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
